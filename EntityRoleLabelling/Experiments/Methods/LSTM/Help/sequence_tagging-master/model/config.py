d5b381ad0939408de37a0ad0915ee5f188a9add4
neelesh <neelesh.shukla@iitg.ernet.in>
2017-11-03 03:55:28 +0530

    entity ranking

--- EntityRoleLabelling/Experiments/Methods/LSTM/Help/sequence_tagging-master/model/config.py
+++ EntityRoleLabelling/Experiments/Methods/LSTM/Help/sequence_tagging-master/model/config.py
@@ -68,7 +68,7 @@ class Config():
     filename_glove = "data/glove.6B/glove.6B.{}d.txt".format(dim_word)
     # trimmed embeddings (created from glove_filename with build_data.py)
     filename_trimmed = "data/glove.6B.{}d.trimmed.npz".format(dim_word)
-    use_pretrained = False
+    use_pretrained = True
 
     # dataset
     # filename_dev = "data/coNLL/eng/eng.testa.iob"
@@ -102,5 +102,5 @@ class Config():
     hidden_size_lstm = 300 # lstm on word embeddings
 
     # NOTE: if both chars and crf, only 1.6x slower on GPU
-    use_crf = True # if crf, training is 1.7x slower on CPU
+    use_crf = False # if crf, training is 1.7x slower on CPU
     use_chars = True # if char embedding, training is 3.5x slower on CPU
